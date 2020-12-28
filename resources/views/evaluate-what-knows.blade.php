f71128cbd78a5fc66128317ac2212caf42de2744
sgallova <sofigava@gmail.com>
2020-12-28 10:05:30 -0500

    Update evaluate-what-knows.blade.php
    Ajusto subtítulo según lo reportado en el error#17 https://docs.google.com/document/d/1W9dGph-3HIVo-aQ67NcRZQ8wrbu7GiGn/edit#

--- resources/views/evaluate-what-knows.blade.php
+++ resources/views/evaluate-what-knows.blade.php
@@ -4,7 +4,7 @@
     <section class="container-question">
         <div class="rd-container">
             <div class="rd-element rd-s-100">
-                <h1 class="text-color-blue-bold title">Evalúe lo que Sabes</h1>
+                <h1 class="text-color-blue-bold title">Evalúe lo que Sabe</h1>
             </div>
             <div class="rd-element rd-s-100">
                 <h2 class="text-color-blue-bold title-300-2em">Los hechos</h2>
