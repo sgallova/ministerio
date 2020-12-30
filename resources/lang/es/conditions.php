9e90e7ccde4fc849a399f4cc3493faf157d7c1ff
sgallova <sofigava@gmail.com>
2020-12-30 12:37:58 -0500

    Update conditions.php
    Modificaciones texto Solicitado por UdeA en revisión del 29 de dic 2020

--- resources/lang/es/conditions.php
+++ resources/lang/es/conditions.php
@@ -9,8 +9,8 @@ return [
        "fifth_question" => "En otros espacios de interacción distintos a las instituciones educativas, en las que participamos con mi(s) hijo(s), aplicamos las medidas de bioseguridad."
     ],
     "learning_study_conditions" => [
-        "first_condition" => "Mi hijo tiene acceso al material de apoyo pedagógico para su estudio y se ha adaptado a la educación es casa.",
-        "second_condition" => "Mi hijo puede combinar el aprendizaje presencial con el aprendizaje es casa.",
+        "first_condition" => "Mi hijo tiene acceso al material de apoyo pedagógico para su estudio y se ha adaptado a la educación en casa.",
+        "second_condition" => "Mi hijo puede combinar el aprendizaje presencial con el aprendizaje en casa.",
     ],
     "working_conditions_parents_caregivers" => [
         "first_condition" => "Puedo trabajar mientras mi hijo no está asistiendo presencial a su institución educativa, es decir, no he tenido que renunciar a mi empleo.",
