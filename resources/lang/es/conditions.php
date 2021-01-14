8e8813aacc47c192ed676c26d046ca8159ee162b
sebastianiserte <77463872+sebastianiserte@users.noreply.github.com>
2021-01-14 15:46:55 -0500

    Update conditions.php
    Ajustes de textos solicitados por el ministerio QA productivo enero 14

--- resources/lang/es/conditions.php
+++ resources/lang/es/conditions.php
@@ -2,8 +2,8 @@
 
 return [
     "biosecurity_conditions" => [
-        "first_condition" => "Conozco el protocolo de bioseguridad que se va a implementar es la institución educativa.",
-        "second_condition" => "Me siento tranquilo con las medidas de bioseguridad que se van a implementar es la institución educativa.",
+        "first_condition" => "Conozco el protocolo de bioseguridad que se va a implementar en la institución educativa.",
+        "second_condition" => "Me siento tranquilo con las medidas de bioseguridad que se van a implementar en la institución educativa.",
         "third_condition" => "Voy a reportar de inmediato a la institución educativa si mi hijo tiene síntomas de COVID-19.",
         "fourth_condition" => "En caso de que mi hijo tenga síntomas de COVID-19, no permitiré que vaya a la institución educativa y consultaré al médico.",
        "fifth_question" => "En otros espacios de interacción distintos a las instituciones educativas, en las que participamos con mi(s) hijo(s), aplicamos las medidas de bioseguridad."
